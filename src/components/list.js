9a536a13b0dcc61f0fcab612455e92571d236781
Alejandro Mcgarvie <mcgarvie.alejandro@gmail.com>
2022-10-26 17:33:25 -0700

    Continued work on artifical api

--- src/components/list.js
+++ src/components/list.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, View } from 'react-native';
-import { colors } from "../utils/colors";
+import React, { useState } from 'react';
+import { Text, StyleSheet, View } from "react-native";
+import { colors } from '../utils/colors';
 
 export const list = ({ subject2 }) => {
   <View style={container.styles}>subject2 = "List";</View>;
